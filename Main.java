087b5bee742ced1b05c3f014c66c156295c13902
MD SAMIN YASAR SAADAT <65817171+SAADAT619@users.noreply.github.com>
2022-05-15 00:29:01 +0000

    array input take in java

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
+import java.util.*;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+ Scanner sc = new Scanner(System.in);          
+    int[] arr = new int[5];
+
+System.out.println("Enter numbers: ");
+  for(int i=0; i<5; i++){
+          arr[i] = sc.nextInt();
+  }
+
+  for(int i: arr){
+          System.out.println(i);
+  }
+
+          
+          
+          
   }
 }
